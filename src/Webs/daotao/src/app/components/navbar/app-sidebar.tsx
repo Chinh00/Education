7f631a5ce2410263f6e26455c3175917939d6f9d
Ching <hanvipno9@gmail.com>
2025-04-25 20:43:57 +0700

    update

--- src/Webs/daotao/src/app/components/navbar/app-sidebar.tsx
+++ src/Webs/daotao/src/app/components/navbar/app-sidebar.tsx
@@ -1,5 +1,5 @@
 
-import {Link, useNavigate} from "react-router";
+import {Link, useLocation, useNavigate} from "react-router";
 import { NavUser } from "./nav-user";
 import {
     Sidebar,
@@ -20,24 +20,25 @@ import {Box, Typography} from "@mui/material";
 
 export function AppSidebar() {
     const nav = useNavigate()
+    const location = useLocation()
     return (
-        <Sidebar className={"w-max h-screen bg-blue-300"} collapsible={"none"} >
+        <Sidebar className={"w-max h-screen bg-emerald-500 "} collapsible={"none"} >
             <SidebarContent className={""}>
                 <SidebarGroup className={"w-full"}>
                     <SidebarMenu className={"flex flex-col justify-center items-center w-full"}>
-                        <SidebarMenuItem className={"cursor-pointer w-full"}>
+                        <SidebarMenuItem className={"cursor-pointer w-full "}>
                             <SidebarMenuButton onClick={() => nav(RoutePaths.HOME_PATH)} className="w-full text-sidebar-foreground/70 flex flex-col h-max cursor-pointer">
-                                <Box><Home size={25} /></Box>
-                                <Typography fontSize={14} fontWeight={"bold"} whiteSpace={"nowrap"}>Trang chủ</Typography>
+                                <Box><Home size={25}  className={"text-white"}/></Box>
+                                <Typography className={"text-white"} fontSize={14} fontWeight={"bold"} whiteSpace={"nowrap"}>Trang chủ</Typography>
                             </SidebarMenuButton>
                         </SidebarMenuItem>
-                        <SidebarMenuItem className={"cursor-pointer w-full"}>
+                        <SidebarMenuItem className={`cursor-pointer w-full ${location.pathname.startsWith("/education") && "bg-white rounded-md"} `}>
                             <SidebarMenuButton onClick={() => nav(RoutePaths.EDUCATION_TRAINING)} className="w-full text-sidebar-foreground/70 flex flex-col h-max cursor-pointer">
                                 <Box><GraduationCap size={25} /></Box>
                                 <Typography fontSize={14} fontWeight={"bold"} whiteSpace={"nowrap"}>Đào tạo</Typography>
                             </SidebarMenuButton>
                         </SidebarMenuItem>
-                        <SidebarMenuItem className={"cursor-pointer w-full"}>
+                        <SidebarMenuItem className={`cursor-pointer w-full ${location.pathname.startsWith("/student") && "bg-white rounded-md"}`}>
                             <SidebarMenuButton onClick={() => nav(RoutePaths.STUDENT_LIST)} className="w-full text-sidebar-foreground/70 flex flex-col h-max cursor-pointer">
                                 <Box><Users size={25} /></Box>
                                 <Typography fontSize={14} fontWeight={"bold"} whiteSpace={"nowrap"}>Sinh viên</Typography>
--- src/Webs/daotao/src/app/modules/education/pages/dashboard_register.tsx
+++ src/Webs/daotao/src/app/modules/education/pages/dashboard_register.tsx
@@ -1,7 +1,19 @@
 import PredataScreen from "@/app/components/screens/predata_screen.tsx";
 import {Box} from "@mui/material";
+import {useAppDispatch, useAppSelector} from "@/app/stores/hook.ts";
+import {CommonState, setGroupFuncName} from "@/app/stores/common_slice.ts";
+import {useEffect} from "react";
+import {useGetRegisterSates} from "@/app/modules/education/hooks/useGetRegisterSates.ts";
 
 const DashboardRegister = () => {
+    const dispatch = useAppDispatch()
+
+    const {groupFuncName} = useAppSelector<CommonState>(c => c.common)
+    useEffect(() => {
+        dispatch(setGroupFuncName({...groupFuncName, itemName: "Thống kê đăng ký học"}));
+    }, []);
+
+
     return <PredataScreen isLoading={false} isSuccess={true} >
         <Box></Box>
     </PredataScreen>

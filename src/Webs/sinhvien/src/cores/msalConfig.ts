b70cefa422b9d88b20c4f43284b8090ba3f066e1
Ching <hanvipno9@gmail.com>
2025-05-28 22:58:28 +0700

    update sinhvien app

--- src/Webs/sinhvien/src/cores/msalConfig.ts
+++ src/Webs/sinhvien/src/cores/msalConfig.ts
@@ -3,10 +3,10 @@ import { PublicClientApplication } from "@azure/msal-browser";
 
 export const msalInstance = new PublicClientApplication({
   auth: {
-    clientId: import.meta.env.CLIENT_ID,
-    authority: `https://login.microsoftonline.com/${import.meta.env.TENANT_ID}`,
-    redirectUri: import.meta.env.REDIRECT_URL,
-    postLogoutRedirectUri: `${import.meta.env.REDIRECT_URL}/login`,
+    clientId: import.meta.env.VITE_CLIENT_ID,
+    authority: `https://login.microsoftonline.com/${import.meta.env.VITE_TENANT_ID}`,
+    redirectUri: import.meta.env.VITE_REDIRECT_URL,
+    postLogoutRedirectUri: `${import.meta.env.VITE_REDIRECT_URL}/login`,
   },
   cache: {
     cacheLocation: "sessionStorage",

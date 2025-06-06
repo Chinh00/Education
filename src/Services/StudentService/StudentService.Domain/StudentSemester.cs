9dc8b39b2ee9f82a57e6ecf851a75fe74fda8687
Ching <hanvipno9@gmail.com>
2025-06-06 13:39:55 +0700

    Update StudentSemester.cs

--- src/Services/StudentService/StudentService.Domain/StudentSemester.cs
+++ src/Services/StudentService/StudentService.Domain/StudentSemester.cs
@@ -1,4 +1,5 @@
 using System.ComponentModel;
+using System.Text.Json.Serialization;
 using Education.Core.Domain;
 
 namespace StudentService.Domain;
@@ -9,7 +10,7 @@ public class StudentSemester : BaseEntity
     public string StudentCode { get; set; }
     public string SemesterCode { get; set; }
     
-    
+    [JsonIgnore]
     public ICollection<SubjectResult> SubjectResults { get; set; } = new List<SubjectResult>();
     public ICollection<string> CourseSubjects { get; set; } = [];
 

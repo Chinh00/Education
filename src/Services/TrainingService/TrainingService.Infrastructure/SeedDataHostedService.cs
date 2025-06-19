77bd5e91e63a23313bc24650a52c88a3c2162735
Ching <hanvipno9@gmail.com>
2025-06-19 23:53:51 +0700

    Update SeedDataHostedService.cs

--- src/Services/TrainingService/TrainingService.Infrastructure/SeedDataHostedService.cs
+++ src/Services/TrainingService/TrainingService.Infrastructure/SeedDataHostedService.cs
@@ -64,7 +64,7 @@ public class SeedDataHostedService(IServiceScopeFactory serviceScopeFactory, Htt
 
     async Task PullRooms(IMongoRepository<Room> roomRepository,CancellationToken cancellation)
     {
-        var url = $"https://api5.tlu.edu.vn/api/Building/Room?Page=1&PageSize=9999";
+        var url = $"https://api5.tlu.edu.vn/api/Building/Room?Page=1&PageSize=9999&Includes=Code&Includes=Name&Includes=Capacity&Includes=BuildingCode&Includes=SupportedConditions";
         
         var response = await httpClient.GetAsync(url, cancellation);
         var json = await response.Content.ReadAsStringAsync(cancellation);
